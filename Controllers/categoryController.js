2854fbb9719adf174d52121a5c9b42848459dd98
sabhishek07 <jollrakeshs.74@gmail.com>
2023-07-28 22:22:44 +0530

    categroy section done

--- Controllers/categoryController.js
+++ Controllers/categoryController.js
@@ -33,11 +33,95 @@ export const createCategoryController =async(req,res)=>{
         }
         
     } catch (error) {
-        console.log(errpr)
+        console.log(error)
         res.status(500).send({
-            sucess:false,
+            success:false,
             message:"error in category"
         })
+    }
+
+}
+
+///update category
+export const updatecategroyController=async()=>{
+    try {
+        const{name}=req.body;
+        const{id}=req.param;
+        const update=await categories.findByIdAndUpdate(id,{name,slug:slugify(name),new:true})
+        res.status(201).send({
+            success:true,
+            message:"update successfully",
+            update
+        })
+        
+    } catch (error) {
+        console.log(error)
+         res.status(500).send({
+            success:false,
+            message:"error in update "
+         })
+    }
+}
+
+//delete
+export const deletecategroyController=async()=>{
+    try {
+        const{id}=req.params;
+       await categories.findByIdAndDelete(id)
+        res.status(201).send({
+            success:true,
+            message:"Delete successfully",
+           
+        })
+        
+    } catch (error) {
+        console.log(error)
+         res.status(500).send({
+            success:false,
+            message:"error in deleting "
+         })
+    }
+}
+//get all categories
+export const getallcategoryController=async()=>{
+
+    try {
+        const getallcategory=await categories.find({})
+        res.status(200).send({
+            success:true,
+            message:"get categories",
+            getallcategory
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:"error in fetching all categories"
+        })
+        
+    }
+
+}
+//getsingle categories
+export const getSinglecategoryController=async()=>{
+
+    try {
+        const {id}=req.params
+        const getSinglecategory=await categories.find({id})
+        res.status(200).send({
+            success:true,
+            message:"get category",
+            getSinglecategory
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:"error in fetching single categories"
+        })
+        
     }
 
 }
--- Routes/category.js
+++ Routes/category.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { isadmin, requiresign } from '../Middlewares/authmiddleware';
-import { createCategoryController } from '../Controllers/categoryController';
+import { createCategoryController, deletecategroyController, getSinglecategoryController, getallcategoryController, updatecategroyController } from '../Controllers/categoryController';
 
 const router =express.Router()
 
 //router.
-router.post('create-category',requiresign,isadmin,createCategoryController)
+router.post('/create-category',requiresign,isadmin,createCategoryController)
+router.put('/updtae-category/:id',requiresign,isadmin,updatecategroyController)
+router.get('/get-categories',getallcategoryController)
+router.get('category-single/:id',getSinglecategoryController)
+router.delete('/delete-category:id',requiresign,isadmin,deletecategroyController)
 
 export default router;
