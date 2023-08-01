b733751bd579482c4e89ab3da65987e4a7571386
sabhishek07 <jollrakeshs.74@gmail.com>
2023-08-01 22:10:28 +0530

    product controller done

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -80,3 +80,86 @@ export const singleProductController=async(req,res)=>{
     }
 
 }
+//getproductphoto
+export const PhotoProductController=async(req,res)=>{
+    try {
+        const{pid}=req.params;
+        const photoUpdate=await products.findById({pid}).select("photo")
+        if(photoUpdate.photo.data){
+            res.set('Content-Type',photoUpdate.photo.contentType)
+            res.status(200).send(photoUpdate.photo.data)
+
+        }
+        
+    } catch (error) {
+        conole.log(error)
+        res.status(500).send({
+            success:false,
+            message:"not getting photo"
+        })
+        
+    }
+   
+    
+
+}
+
+//delete product
+export const deleteProductController=async(req,res)=>{
+    try {
+        const{pid}=req.params;
+        await products.findByIdAndDelete(pid).select("-photo")
+        res.status(200).send({
+            success:true,
+            message:"product delete succesfully"
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(
+            {
+                success:false,
+                message:"error while deleting"
+            }
+        )
+    }
+
+}
+
+//update product
+export const UpdateproductController=async(req,res)=>{
+    try{
+        const{name,description,category,slug,quantity,price,shiping}=req.fields;
+        const{photo}=req.files;
+        if(!name||!description||!slug || !quantity || !price ||!category ){
+            res.status(500).send({
+                message:"all fields are required"
+            })
+        }
+            if(photo && photo.size>10000000){
+                res.status(500).send({
+                    message:"file should not be more than 10 mb"
+                })
+            }
+            const{pid}=req.params
+            const products= await products.findByIdAndUpdate(pid,{...fields,slug:slugify(name)},{new:true})
+            if(photo){
+                products.photo.data=fs.readFileSync(photo.path)
+                products.photo.contentType=photo.type
+            }
+            await products.save();
+            res.satus(201).send({
+                success:true,
+                message:"product updated successfully",
+                products
+            })
+        }
+        catch(error){
+            console.log(error)
+            res.satus(500).send({
+                success:false,
+                message:"error in while creating products"
+            })
+
+}
+}
--- Routes/productroutes.js
+++ Routes/productroutes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { isadmin, requiresign } from "../Middlewares/authmiddleware";
-import { createproductController, getallproductController, singleProductController } from "../Controllers/ProductController";
+import { PhotoProductController, UpdateproductController, createproductController, deleteProductController, getallproductController, singleProductController } from "../Controllers/ProductController";
 import formidable from 'express-formidable'
 
 
 const router =express.Router()
 
 router.post('/create-product',requiresign,isadmin,formidable(),createproductController)
+router.post('/update-product/:pid',requiresign,isadmin,formidable(),UpdateproductController)
 router.get('/get-product',getallproductController)
 router.get('/get-product-single/:id',singleProductController)
-
+router.get('/get-photo/:pid',PhotoProductController)
+router.delete('/delete-product/:pid',deleteProductController)
 export default router;
