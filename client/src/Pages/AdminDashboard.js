9f43e2c80d5ef4d68072d61ee00370257c0bc7cf
sabhishek07 <jollrakeshs.74@gmail.com>
2023-08-07 23:14:06 +0530

    working on admin dashboard

--- client/src/Pages/AdminDashboard.js
+++ client/src/Pages/AdminDashboard.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Adminmenu from './Adminmenu.js'
 
 export default function AdminDashboard() {
   return (
     <div>
-      admin
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col md-3'>
+            Admin Pannel
+          <Adminmenu/>
+          </div>
+         
+          <div className='col md-9'>
+            content
+          </div>
+
+        </div>
+      </div>
       
     </div>
   )
---
+++ client/src/Pages/Adminmenu.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+const Adminmenu = () => {
+  return (
+    <div>
+    <div className="list-group">
+ 
+  <Link to=""className="list-group-item">Create Category</Link>
+  <Link to=""className="list-group-item">Create Product</Link>
+  <Link to=""className="list-group-item">Users</Link>
+</div>
+
+    </div>
+  )
+}
+
+export default Adminmenu
